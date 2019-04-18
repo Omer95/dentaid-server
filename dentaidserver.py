0bfeb26b7e0a0e5fc10bbe87bce8ede35752f253
Mirza. Beg <omerfarooqali@gmail.com>
2019-04-19 04:18:26 +0500

    properly decoded image byte stream to write to file and upload to firebase

--- dentaidserver.py
+++ dentaidserver.py
@@ -5,6 +5,8 @@ import cv2
 from matplotlib import pyplot as plt
 import firebase_admin
 from firebase_admin import credentials, storage
+import json
+import base64
 #init app
 app = Flask(__name__)
 # init firebase admin and storage bucket
@@ -17,17 +19,16 @@ bucket = storage.bucket()
 @app.route("/", methods=["GET","POST"])
 def hello():
     if request.method == 'POST':
-        # convert string of image data to uint8
-        nparr = np.frombuffer(request.data, np.uint8)
-        # decode image
+        payload = request.get_json()     
+        # write the image to file
+        img_str = payload.get('image')
+        img_bytes = img_str.encode('utf-8')
+        nparr = np.frombuffer(base64.b64decode(img_bytes), np.uint8)
         img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        # to-do: run test image through
-        # save the image to local directory
         cv2.imwrite('temp.jpg', img)
         # upload image to firebase storage bucket
-        blob = bucket.blob('new_image.jpg') 
+        blob = bucket.blob('opgclient/' + payload.get('filename')) 
         blob.upload_from_filename('./temp.jpg')
-        print(img)
         return jsonify({
             "response": "Image received and uploaded to firebase"
         })
